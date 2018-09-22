ed31d3435783420939ee487b841ac8f503e6dd74
Jacob Schwartz <jtschwartz98@gmail.com>
2018-09-21 22:47:06 -0400

    Handles mobile more efficiently

--- js/rootComponents.js
+++ js/rootComponents.js
@@ -44,6 +44,7 @@ $(function () {
         '                </ul>\n' +
         '                <ul class="navbar-nav d-md-none">\n' +
         '                    <li class="nav-item">\n' +
+        '                        <hr/>\n' +
         '                        <a target="_blank" class="nav-link pr-2" style="display: inline-block !important;" href="mailto:jacob@jtschwartz.com"><i class="fas fa-envelope-square fa-2x"></i></a>\n' +
         '                        <a target="_blank" class="nav-link px-2" style="display: inline-block !important;" href="https://www.linkedin.com/in/jacob-t-schwartz"><i class="fab fa-linkedin fa-2x"></i></a>\n' +
         '                        <a target="_blank" class="nav-link px-2" style="display: inline-block !important;" href="https://github.com/JTSchwartz/"><i class="fab fa-github-square fa-2x"></i></a>\n' +
