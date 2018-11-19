2b5522c97325611d6873906948cb97d471a8c05b
Jacob Schwartz <jtschwartz98@gmail.com>
2018-11-19 00:13:05 -0500

    TraceRT added

--- js/rootComponents.js
+++ js/rootComponents.js
@@ -17,6 +17,7 @@ $(function () {
 		'                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownCurrent" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Current Projects</a>\n' +
 		'                        <div class="dropdown-menu" aria-labelledby="navbarDropdownCurrent">\n' +
 		'							  <a class="dropdown-item" href="https://jtschwartz.com/Chess/">Chess</a>\n' +
+		'							  <a class="dropdown-item" href="https://jtschwartz.com/TraceRT/">TraceRT</a>\n' +
 		'                            <a id="emptyCur" class="dropdown-item disabled d-none" href="#">Creating a To Do List...</a>\n' +
 		'                        </div>\n' +
 		'                    </li>\n' +
